068e16676e821e186546b5cc9c300fc542f9a88c
VenkateshRoshan <42509229+VenkateshRoshan@users.noreply.github.com>
2024-09-29 20:53:38 -0400

    Code Updation

--- mlfow/mlflow.py
+++ mlfow/mlflow.py
@@ -2,6 +2,8 @@ import mlflow
 import os
 import sys
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
 from model import SimpleCNN
 import torch
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,12 @@
 import pytest
 import json
+import sys
+import os
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+
 from app import app
 
 def test_predict():
